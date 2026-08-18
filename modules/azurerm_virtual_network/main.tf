6f5eb5b3748048e2344034f8302d1220f2a1aa35
Ravinder Singhmar <Ravinder.Singhmar@gmail.com>
2026-08-18 22:42:21 +0530

    add TFLint

--- modules/azurerm_virtual_network/main.tf
+++ modules/azurerm_virtual_network/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "dev-vnet" {
-  for_each = var.vnetdetails
+  for_each            = var.vnetdetails
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
   location            = each.value.location
